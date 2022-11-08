041c44f6b1d3405ef3f33e46738ad323a2897675
Chavez, Andres <andres.chavez@dish.com>
2022-11-08 17:06:48 -0600

    testing...

--- static/scripts.js
+++ static/scripts.js
@@ -236,7 +236,7 @@ async function getStandings()
         var requestOptions = { method: 'GET',  redirect: 'follow', origin: '*'};
         
 
-    let response = await fetch("https://sozmwkhko9.execute-api.us-west-2.amazonaws.com/default/test", requestOptions);
+    let response = await fetch("https://i57hmdn6xa.execute-api.us-west-2.amazonaws.com/default/test?url=https://api.sportradar.com/americanfootball/trial/v2/en/seasons/sr:season:90233/standings.json?round=1&api_key=" + localStorage.getItem("API_KEY"), requestOptions);
       //
         //let response = await fetch("http://localhost:3000/fetch/https://api.sportradar.com/americanfootball/trial/v2/en/seasons/sr:season:90233/standings.json?round=1&api_key=" + localStorage.getItem("API_KEY"), requestOptions);
       //let response = await fetch("https://api.sportradar.com/americanfootball/trial/v2/en/seasons/sr:season:90233/standings.json?round=1&api_key=" + localStorage.getItem("API_KEY"), requestOptions);
@@ -321,7 +321,8 @@ async function getProbabilities()
     //var requestOptions = {method: 'GET', redirect: 'follow', 'Access-Control-Allow-Origin': 'https://api.sportradar.com', referrerPolicy: 'origin-when-cross-origin', mode: 'no-cors'};
     var requestOptions = { method: 'GET',  redirect: 'follow', origin: '*'};
     
-    let response = await fetch("http://localhost:3000/fetch/https://api.sportradar.com/americanfootball/trial/v2/en/seasons/sr:season:90233/probabilities.json?api_key=" + localStorage.getItem("API_KEY") , requestOptions);
+    // let response = await fetch("http://localhost:3000/fetch/https://api.sportradar.com/americanfootball/trial/v2/en/seasons/sr:season:90233/probabilities.json?api_key=" + localStorage.getItem("API_KEY") , requestOptions);
+    let response = await fetch("https://i57hmdn6xa.execute-api.us-west-2.amazonaws.com/default/test?url=https://api.sportradar.com/americanfootball/trial/v2/en/seasons/sr:season:90233/probabilities.json?api_key=" + localStorage.getItem("API_KEY"), requestOptions);
 
     //let response = await fetch("https://api.sportradar.com/americanfootball/trial/v2/en/seasons/sr:season:90233/probabilities.json?api_key=" + localStorage.getItem("API_KEY") , requestOptions);
         let data = await response.json();
@@ -371,7 +372,7 @@ async function getScores()
 {
     var requestOptions = { method: 'GET',  redirect: 'follow', origin: '*'};
     //http://localhost:3000/fetch/
-    let response = await fetch("https://sozmwkhko9.execute-api.us-west-2.amazonaws.com/default/test"  , requestOptions);
+    let response = await fetch("https://i57hmdn6xa.execute-api.us-west-2.amazonaws.com/default/test?url=https://api.sportradar.com/americanfootball/trial/v2/en/schedules/live/summaries.json?api_key=" + localStorage.getItem("API_KEY" ), requestOptions);
     // let response = await fetch("http://localhost:3000/fetch/https://api.sportradar.com/americanfootball/trial/v2/en/schedules/live/summaries.json?api_key=" + localStorage.getItem("API_KEY" ) , requestOptions);
     
     //let response = await fetch("https://api.sportradar.com/americanfootball/trial/v2/en/schedules/live/summaries.json?api_key=" + localStorage.getItem("API_KEY" ) , requestOptions);
